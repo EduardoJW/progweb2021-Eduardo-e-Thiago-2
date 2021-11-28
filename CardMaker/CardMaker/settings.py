1a70d0c5dc987d68fc80edf865bb0b4a047153e5
Pbmn10 <pbmn10@gmail.com>
2021-11-28 18:57:43 -0300

    Adjustments for it to work on Heroku
    Some new files and small changes to Settings.py

--- CardMaker/CardMaker/settings.py
+++ CardMaker/CardMaker/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#^7*q!$*_1h49+h205eqb$$2dek&f^q9wv^l33qbo)j@6#^s2+'
+#SECRET_KEY = 'django-insecure-#^7*q!$*_1h49+h205eqb$$2dek&f^q9wv^l33qbo)j@6#^s2+'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','card-maker-app.herokuapp.com']
 
 
 # Application definition
@@ -120,7 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'   #/static/
+
+
 STATICFILES_DIR = [
     os.path.join(BASE_DIR, 'CardMakerApp/static'),
 
